c6226ac052db0f28b68a8cb6a25dadd66c8fd8b4
Daniel Henrique Ferreira Carvalho <danielferreira.carvalho@philips.com>
2026-07-16 17:07:34 -0300

    Change some core emacs configurations

--- init.el
+++ init.el
@@ -1,26 +1,135 @@
-;;Set up the package manager
+;; Initialize package sources
 (require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;;Set up use-package
-(when (< emacs-major-version 29)
-  (unless (package-installed-p 'use-package)
-    (unless package-archive-contents
-      (package-refresh-contents))
-    (package-install 'use-package)))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;;Do not show warnings when installing packages
-(add-to-list 'display-buffer-alist
-             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
-               (display-buffer-no-window)
-               (allow-no-window . t)))
+;; Core settings
+(setq ;; Yes, this is Emacs
+      inhibit-startup-message t
+
+      ;; Instruct auto-save-mode to save to the current file, not a backup file
+      auto-save-default nil
+
+      ;; No backup files, please
+      make-backup-files nil
+
+      ;; Make it easy to cycle through previous items in the mark ring
+      set-mark-command-repeat-pop t
+
+      ;; Don't warn on large files
+      large-file-warning-threshold nil
+
+      ;; Don't warn on advice
+      ad-redefinition-action 'accept
+
+      ;; Revert Dired and other buffers
+      global-auto-revert-non-file-buffers t
+
+      ;; Silence compiler warnings as they can be pretty disruptive
+      native-comp-async-report-warnings-errors nil)
+
+;; Tabs to spaces
+(setq-default indent-tabs-mode nil
+	      tab-width 2)
+
+(setopt tab-always-indent 'complete
+        read-buffer-completion-ignore-case t
+        read-file-name-completion-ignore-case t
+
+        ;; This *may* need to be set to 'always just so that you don't
+        ;; miss other possible good completions that match the input
+        ;; string.
+        completion-auto-help t
+
+        ;; Include more information with completion listings
+        completions-detailed t
+
+        ;; Move focus to the completions window after hitting tab
+        ;; twice.
+        completion-auto-select 'second-tab
+
+        ;; If there are 3 or less completion candidates, don't pop up
+        ;; a window, just cycle through them.
+        completion-cycle-threshold 3
+
+        ;; Cycle through completion options vertically, not
+        ;; horizontally.
+        completions-format 'vertical
+
+        ;; Sort recently used completions first.
+        completions-sort 'historical
+
+        ;; Only show up to 10 lines in the completions window.
+        completions-max-height 10
+
+        ;; Don't show the unneeded help string at the top of the
+        ;; completions buffer.
+        completion-show-help nil
+
+        ;; Add more `completion-styles' to improve candidate selection.
+        completion-styles '(basic partial-completion substring initials))
+
+(keymap-set minibuffer-local-map "C-p" #'minibuffer-previous-completion)
+(keymap-set minibuffer-local-map "C-n" #'minibuffer-next-completion)
+
+(use-package emacs-solo-rainbow-delimiters
+  :ensure nil
+  :no-require t
+  :defer t
+  :init
+  (defun emacs-solo/rainbow-delimiters ()
+    "Apply simple rainbow coloring to parentheses, brackets, and braces in the current buffer.
+Opening and closing delimiters will have matching colors."
+    (interactive)
+    (let ((colors '(font-lock-keyword-face
+                    font-lock-type-face
+                    font-lock-function-name-face
+                    font-lock-variable-name-face
+                    font-lock-constant-face
+                    font-lock-builtin-face
+                    font-lock-string-face
+                    )))
+      (font-lock-add-keywords
+       nil
+       `((,(rx (or "(" ")" "[" "]" "{" "}"))
+          (0 (let* ((char (char-after (match-beginning 0)))
+                    (depth (save-excursion
+                             ;; Move to the correct position based on opening/closing delimiter
+                             (if (member char '(?\) ?\] ?\}))
+                                 (progn
+                                   (backward-char) ;; Move to the opening delimiter
+                                   (car (syntax-ppss)))
+                               (car (syntax-ppss)))))
+                    (face (nth (mod depth ,(length colors)) ',colors)))
+               (list 'face face)))))))
+    (font-lock-flush)
+    (font-lock-ensure))
+
+  (add-hook 'prog-mode-hook #'emacs-solo/rainbow-delimiters))
 
 ;Delete the selected text upon text insertion
 (use-package delsel
   :ensure nil
   :hook (after-init . delete-selection-mode))
+
+(setq inhibit-startup-message t)
+(tooltip-mode -1)           ; Disable tooltips
+
+;; Set up the visible bell
+(setq visible-bell t)
 
 ;;Load black theme
 (load-theme 'modus-vivendi)
@@ -46,6 +155,19 @@
 ;;Start in fullscreen mode
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
 
+(savehist-mode 1)              ;; Save minibuffer history
+(column-number-mode 1)         ;; Show column number on mode line
+(global-visual-line-mode 1)    ;; Visually wrap long lines in all buffers
+
+;; Display line numbers in programming modes
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; Delete trailing whitespace before saving buffers
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Make vertical window separators look nicer in terminal Emacs
+(set-display-table-slot standard-display-table 'vertical-border (make-glyph-code ?│))
+
 ;;Show commands description
 (use-package marginalia
   :ensure t
@@ -65,8 +187,6 @@
   :hook (after-init . savehist-mode))
 (custom-set-variables
  '(package-selected-packages '(orderless marginalia)))
-(custom-set-faces.
- )
 
 ;;Set up magit
 (use-package magit
