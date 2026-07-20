f67ef8c3c422340b72cf5a9ef9378035adb9a282
Daniel Henrique <danielhfc01@gmail.com>
2026-07-20 16:39:17 -0300

    Remove head from merge

--- init.el
+++ init.el
@@ -207,7 +207,7 @@
 (use-package savehist
   :ensure nil
   :hook (after-init . savehist-mode))
-<<<<<<< HEAD
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
